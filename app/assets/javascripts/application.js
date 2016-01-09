27d440c3a0015b0266deec7ad284d9bce630a0b8
Isaac Agutey <isaacagutey@gmail.com>
2016-01-09 09:19:05 -0500

    front-end-good

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery.menu-aim.js
 //= require modernizr.js
-//= require main.js
-//= require jquery.mixitup.min.js
+// require jquery.menu-aim.js
+// require main.js
+// require jquery.mixitup.min.js
 //= require filter_main.js
+
 // require_tree .
